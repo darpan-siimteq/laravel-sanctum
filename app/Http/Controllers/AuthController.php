0e0c4a60f4f9a97e62fe18d545e059378d24a807
darpan-siimteq <darpan.yevale@siimteq.com>
2024-12-05 16:25:47 +0530

    Fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
-class AuthController extends Controller
+class AuthController
 {
     public function register(Request $request)
     {
